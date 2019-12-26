40e75b89eea2fe15b415234149acb606ba641a15
Gabriel Fidelis de Sousa <gabriel.fidelis.sousa@gmail.com>
2019-12-26 20:40:43 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,5 +1,9 @@
 def find_element_index(array, value_to_find)
-  
+  array.length.tomes do |count|
+    if array[count] == value_to_find
+      return count
+    end
+    nill
 end
 
 def find_max_value(array)

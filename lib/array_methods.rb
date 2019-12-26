736ef056249211dec4deb5466ab4c43a02545a95
Gabriel Fidelis de Sousa <gabriel.fidelis.sousa@gmail.com>
2019-12-26 21:17:34 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -21,11 +21,12 @@ end
 
 def find_min_value(array)
  x = 0 
- array.length.times do |index|
+ array.length.times { |index|
    if array[index] < x 
      x = array[index]
    end 
-end
+ }
 x 
 end
+
 

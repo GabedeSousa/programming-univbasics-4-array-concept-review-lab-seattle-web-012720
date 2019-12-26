aebeb50b70847b29a0df06af11e8f95468eefb44
Gabriel Fidelis de Sousa <gabriel.fidelis.sousa@gmail.com>
2019-12-26 20:45:02 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,9 +2,9 @@ def find_element_index(array, value_to_find)
   array.length.times do |count|
     if array[count] == value_to_find
       return count
-    end
     nill
   end
+  
 
 def find_max_value(array)
   # Add your solution here
